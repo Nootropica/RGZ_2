12d88378381ba97abe11f5e93f5261df0a8673bd
Владислав Печенкин <mogucci2000@list.ru>
2024-12-20 19:58:12 +0700

    Доработки

--- app.py
+++ app.py
@@ -27,21 +27,9 @@ def allowed_file(filename):
 
 # Функция для подключения к базе данных
 def db_connect():
-    if current_app.config['DB_TYPE'] == 'postgres':
-        conn = psycopg2.connect(
-            host='127.0.0.1',
-            database='RGZ_BACK',
-            user='rgz_back',
-            password='123'
-        )
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-    else:
-        dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-
+    conn = sqlite3.connect('vladislavpechenkin/RGZ_2/database.db')
+    conn.row_factory = sqlite3.Row 
+    cur = conn.cursor()
     return conn, cur
 
 # Функция для закрытия соединения с базой данных
